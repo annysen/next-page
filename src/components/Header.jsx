e021bae0245b953e2b8f10acbae01e0d3552078c
annysen <annysen95@gmail.com>
2023-08-07 23:33:26 +0600

    partial nav section complite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import { Bars3BottomRightIcon, BoltIcon } from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="bg-gray-200 p-4 mx-auto md:max-w-screen-xl ">
-      <div className="">
+      <div className="flex justify-between items-center">
         {/* icon and title  */}
-        <div className="flex items-center">
+        <Link to="/" className="flex items-center">
           <BoltIcon className="w-6 h-6 text-blue-600" />
           <span className="text-2xl ml-2 font-bold ">nextPage</span>
-        </div>
+        </Link>
         {/* nav item  */}
-        <div></div>
-        {/* responsive nav section  */}
-        <div></div>
+        <ul className="hidden md:flex space-x-8 font-bold">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/books"
+              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
+            >
+              Books
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "text-blue-600" : "")}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+        {/* responsive nav section for mobile */}
+        <div className="md:hidden">{/* dropdown btn  */}</div>
       </div>
     </nav>
   );
