4bea27d607cba8e4742f2e1173928518332beb37
annysen <annysen95@gmail.com>
2023-08-07 23:59:38 +0600

    nav section complite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { Bars3BottomRightIcon, BoltIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import {
+  Bars3BottomRightIcon,
+  XMarkIcon,
+  BoltIcon,
+} from "@heroicons/react/24/solid";
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="bg-gray-200 p-4 mx-auto md:max-w-screen-xl ">
       <div className="flex justify-between items-center">
@@ -11,6 +16,7 @@ const Header = () => {
           <BoltIcon className="w-6 h-6 text-blue-600" />
           <span className="text-2xl ml-2 font-bold ">nextPage</span>
         </Link>
+
         {/* nav item  */}
         <ul className="hidden md:flex space-x-8 font-bold">
           <li>
@@ -38,8 +44,73 @@ const Header = () => {
             </NavLink>
           </li>
         </ul>
+
         {/* responsive nav section for mobile */}
-        <div className="md:hidden">{/* dropdown btn  */}</div>
+        <div className="md:hidden">
+          {/* dropdown btn  */}
+          <button onClick={() => setIsOpen(true)}>
+            <Bars3BottomRightIcon className="w-5 text-gray-500" />
+          </button>
+
+          {isOpen && (
+            <div className=" top-0 left-0 w-full z-10 absolute">
+              <div className="bg-gray-300 p-4">
+                {/* logo and cross btn  */}
+                <div className="flex justify-between items-center">
+                  {/* icon and title  */}
+                  <div>
+                    <Link to="/" className="flex items-center">
+                      <BoltIcon className="w-6 h-6 text-blue-600" />
+                      <span className="text-2xl ml-2 font-bold ">nextPage</span>
+                    </Link>
+                  </div>
+                  {/* drodown Cross btn  */}
+                  <div>
+                    <button onClick={() => setIsOpen(false)}>
+                      <XMarkIcon className="w-5 text-gray-500" />
+                    </button>
+                  </div>
+                </div>
+
+                {/* nav item  */}
+                <div className="mt-4">
+                  <ul className="space-y-2 font-bold">
+                    <li>
+                      <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                          isActive ? "text-blue-600" : ""
+                        }
+                      >
+                        Home
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/books"
+                        className={({ isActive }) =>
+                          isActive ? "text-blue-600" : ""
+                        }
+                      >
+                        Books
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                          isActive ? "text-blue-600" : ""
+                        }
+                      >
+                        About
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
